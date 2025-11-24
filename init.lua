35084f0bc6e836d372f9c96e0c7b4865826f546a
WahPlus <wahplus@proton.me>
2025-11-24 17:34:27 +0200

    Added HTML language server

--- init.lua
+++ init.lua
@@ -676,6 +676,7 @@ require('lazy').setup({
         -- pyright = {},
         rust_analyzer = {},
         tombi = {},
+        html = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
